90c2ddfeb49fd7ebf40ca13cf55c1c7d102f9f8b
Thomas Lohnert <thomas.lohnert@stfc.ac.uk>
2018-01-30 16:39:46 +0000

    Python 3 style print statements

--- testing/genie_python_blocks.py
+++ testing/genie_python_blocks.py
@@ -35,20 +35,20 @@ def genie_python_blocks_test():
                 if i < steps-1:
                     time.sleep(float(max_wait)/float(steps))
                 else:
-                    print "Expected value: {}, {}, {}, {}, {}. Actual value: {}, {}, {}, {}, {} (val, connected, rc, low, high)".format(
+                    print("Expected value: {}, {}, {}, {}, {}. Actual value: {}, {}, {}, {}, {} (val, connected, rc, low, high)".format(
                         value, bool_to_str(connected), bool_to_str(runcontrol), lowlimit, highlimit,
-                        block['value'], block['connected'], block['runcontrol'], block['lowlimit'], block['highlimit'])
+                        block['value'], block['connected'], block['runcontrol'], block['lowlimit'], block['highlimit']))
                     assert False
                     
     def assert_from_user_input(question):  
         assert (raw_input("{}? (Y/N) ".format(question)).lower()+"n")[0]=="y"
         
 
-    print "Test required blocks exist"
+    print("Test required blocks exist")
     assert num_block_name in g.get_blocks()
     assert str_block_name in g.get_blocks()
     
-    print "Test can set block values"
+    print("Test can set block values")
     new_str_value = "TEST"
     g.cset(str_block_name, new_str_value)
     assert g.cget(str_block_name)['value']==new_str_value
@@ -57,7 +57,7 @@ def genie_python_blocks_test():
     g.cset(num_block_name, new_num_value)
     assert_numeric_block(num_block_name, value=new_num_value)
     
-    print "Test setting run control"
+    print("Test setting run control")
     starting_value = 0.0
     lowlimit = -1.0
     highlimit = 1.0
@@ -69,11 +69,11 @@ def genie_python_blocks_test():
     assert_from_user_input("Is TEST_BLOCK_NUM displaying outside its run control limit")
     g.cset(num_block_name, starting_value)
     
-    print "Test show block data"
+    print("Test show block data")
     g.cshow(num_block_name)
     assert_from_user_input("Does the data above look sensible for TEST_BLOCK_NUM")
     g.cshow(str_block_name)
     assert_from_user_input("Does the data above look sensible for TEST_BLOCK_STR")
     
-    print "TESTS COMPLETED SUCCESSFULLY"
+    print("TESTS COMPLETED SUCCESSFULLY")
     
--- testing/genie_python_dae.py
+++ testing/genie_python_dae.py
@@ -9,18 +9,18 @@ def genie_python_dae_test():
     def assert_from_user_input(question):  
         assert (raw_input("{}? (Y/N) ".format(question)).lower()+"n")[0]=="y"
         
-    print "Test get/set title"
+    print("Test get/set title")
     new_title = "NEW TITLE"
     g.change_title(new_title)
     assert g.get_title()==new_title
     
-    print "Test get/set users"
+    print("Test get/set users")
     new_users = "Adrian, Kevin, Kathryn"
     g.change_users(new_users)
     # DAE replaces final "," with "and"
     assert g.get_dashboard()['user'].replace(" and", ",")==new_users
     
-    print "Test change run state"
+    print("Test change run state")
     # Use a range of verbosities
     assert g.get_runstate()=="SETUP"
     g.begin(True)
@@ -36,10 +36,10 @@ def genie_python_dae_test():
     g.end(False)
     assert g.get_runstate()=="SETUP"
 
-    print "Testing waitfors"    
+    print("Testing waitfors")
     g.waitfor_time(seconds=0.1)
     g.waitfor_uamps(0.1)
     g.waitfor_frames(1)
     
-    print "TESTS COMPLETED SUCCESSFULLY"
+    print("TESTS COMPLETED SUCCESSFULLY")
     
